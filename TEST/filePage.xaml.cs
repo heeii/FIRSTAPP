e81fa20816b38349a4c03e4142e6fb51d06d7b9e
heeii <72515845+heeii@users.noreply.github.com>
2024-01-01 23:59:12 +0500

    file_page_class_reqw

--- TEST/filePage.xaml.cs
+++ TEST/filePage.xaml.cs
@@ -1,11 +1,49 @@
 namespace TEST
 {
+    
+    public partial class filePage : ContentPage
+    {
+        private readonly FileService _fileService;
 
-	public partial class filePage : ContentPage
-	{
-		public filePage()
-		{
-			InitializeComponent();
-		}
-	}
+        public filePage()
+        {
+            InitializeComponent();
+            _fileService = new FileService();
+            GetFiles();
+        }
+
+        private async void GetFiles()
+        {
+            var files = await _fileService.GetFilesAsync();
+            FileListView.ItemsSource = files;
+        }
+    }
+    public class FileModel
+    {
+        public string FileName { get; set; }
+        public string FileUrl { get; set; }
+    }
+    public class FileService
+    {
+        private readonly HttpClient _httpClient;
+
+        public FileService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<FileModel>> GetFilesAsync()
+        {
+            var response = await _httpClient.GetAsync("your_server_url_here");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var fileNames = await response.Content.ReadAsStringAsync();
+                var files = fileNames.Split(',').Select(fileName => new FileModel { FileName = fileName, FileUrl = "your_file_url_here" }).ToList();
+                return files;
+            }
+
+            return null;
+        }
+    }
 }
